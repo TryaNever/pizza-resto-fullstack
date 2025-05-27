b95eb389634a348a3b33630ae351eefefd2c1a20
TryaNever <alexispernette@outlook.fr>
2025-05-27 15:15:40 +0200

    Add component CardProduct

---
+++ frontend/src/components/CardProduct.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import StarsRating from "./StarsRating.jsx";
+
+const CardProduct = (item,key) => {
+    item = item.item
+    console.log(item);
+    return (
+        <div key={key}>
+            <i className="ri-heart-line"></i>
+            <div><i className="ri-shopping-cart-2-line"></i></div>
+            <img src="../../public/exemple_product.jpg" alt={item.description}/>
+            <div>
+                <h3>{item.name}</h3>
+                <strong>$ <span>{item.price}</span></strong>
+                <StarsRating rating='80'/>
+                <div><a href={`/product/${item.id}`}>read more</a></div>
+            </div>
+        </div>
+    )
+}
+
+export default CardProduct;
--- frontend/src/components/LoadHomeData.jsx
+++ frontend/src/components/LoadHomeData.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import StarsRating from "./StarsRating.jsx";
+import CardProduct from "./CardProduct.jsx";
 
 export default function LoadHomeData() {
     const [data, setData] = useState([]);
@@ -35,16 +36,7 @@ export default function LoadHomeData() {
             </div>
             <div>
                 {allProduct.slice(0, 3).map((item, index) => (
-                    <div key={index}>
-                        <i className="ri-heart-line"></i>
-                        <div><i className="ri-shopping-cart-2-line"></i></div>
-                        <img src={item.image} alt={item.description}/>
-                        <div>
-                            <h3></h3>
-                            <strong></strong>
-                            <StarsRating rating='80' />
-                        </div>
-                    </div>
+                    <CardProduct key={index} item={item} />
                 ))}
             </div>
         </div>
